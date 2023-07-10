530c45d8337cbf41c718ea1024918c72e845ad63
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2023-07-10 10:36:38 -0300

    alteração controller e index

--- myfinance-web-netcore/Controllers/PlanoContaController.cs
+++ myfinance-web-netcore/Controllers/PlanoContaController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using myfinance_web_netcore.Domain;
 using myfinance_web_netcore.Models;
 
 namespace myfinance_web_netcore.Controllers
 {
+    [Route("[controller]")]
     public class PlanoContaController : Controller
     {
         private readonly ILogger<PlanoContaController> _logger;
@@ -16,14 +18,15 @@ namespace myfinance_web_netcore.Controllers
             _myFinanceDbContext = myFinanceDbContext;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             var listaPlanoContas = _myFinanceDbContext.PlanoConta;
             var listaPlanoContaModel = new List<PlanoContaModel>();
 
-            foreach (var item in listaPlanoContas)
-            {
-                var planoContaModel = new PlanoContaModel(){
+            foreach (var item in listaPlanoContas){
+                var planoContaModel = new PlanoContaModel()
+                {
                     Id = item.Id,
                     Descricao = item.Descricao,
                     Tipo = item.Tipo
@@ -37,9 +40,29 @@ namespace myfinance_web_netcore.Controllers
             return View();
         }
 
-        public IActionResult Cadastro()
+        [HttpGet]
+        [Route("Cadastro")]
+        [Route("Cadastro/{id}")]
+        public IActionResult Cadastro(int? id)
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("Cadastro")]
+        [Route("Cadastro/{input}")]
+        public IActionResult Cadastro(PlanoContaModel input)
+        {
+            var planoConta = new PlanoConta(){
+                Id = input.Id,
+                Descricao = input.Descricao,
+                Tipo = input.Tipo
+            };
+
+            _myFinanceDbContext.PlanoConta.Add(planoConta);
+            _myFinanceDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
